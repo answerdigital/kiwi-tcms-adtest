6713f1150b972c0948c35a313b1505de2af1f38d
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-05-18 12:19:50 +0100

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,123 @@
+terraform {
+  backend "s3" {
+    bucket         = "test-team-kiwi-tcs-dev-state-management-bucket"
+    key            = "global/dev/s3/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "test-team-kiwi-tcs-tf-locks-dev"
+    encrypt        = true
+  }
+}
+
+locals {
+  owner = "Robert Gaskin"
+  project = "test-team-kiwi-TCMS"
+}
+
+module "vpc_subnet" {
+  source               = "github.com/answerdigital/terraform-modules//modules/aws/vpc?ref=v2.3.0"
+  owner                = local.owner
+  project_name         = local.project
+  enable_vpc_flow_logs = false
+}
+
+data "aws_ami" "ec2_ami" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*-x86_64-ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  owners = ["amazon"]
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+resource "aws_security_group" "aws_sg" {
+  name        = "${local.project}-sg"
+  description = "A Basic Security Group"
+  vpc_id      = module.vpc_subnet.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = local.project
+  }
+}
+
+module "ec2_instance_setup" {
+  source                 = "github.com/answerdigital/terraform-modules//modules/aws/ec2?ref=TERRA-EC2Hack"
+  project_name           = local.project
+  owner                  = local.owner
+  ami_id                 = data.aws_ami.ec2_ami.id
+  availability_zone      = data.aws_availability_zones.available.names[0]
+  subnet_id              = module.vpc_subnet.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.aws_sg.id]
+  needs_elastic_ip       = true
+
+  user_data = <<-EOF
+#!/bin/bash -xe
+#logs all user_data commands into a user-data.log file
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -aG docker ec2-user
+sudo newgrp docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sleep 10
+
+git clone --depth 1 https://github.com/kiwitcms/Kiwi.git path/to/kiwi-tcms/
+cd path/to/kiwi-tcms
+
+/usr/local/bin/docker-compose up
+
+EOF
+
+}
