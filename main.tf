1cb00131cce5d7adb9ec9086502db48a33ad04a5
Robert Gaskin <robert.gaskin@answerdigital.com>
2023-06-05 11:40:54 +0100

    Use latest version of Modules, and improve naming of Modules

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
 terraform {
   backend "s3" {
     bucket         = "test-team-kiwi-tcs-dev-state-management-bucket"
@@ -9,12 +13,12 @@ terraform {
 }
 
 locals {
-  owner = "Robert Gaskin"
+  owner   = "Robert Gaskin"
   project = "test-team-kiwi-TCMS"
 }
 
 module "vpc_subnet" {
-  source               = "github.com/answerdigital/terraform-modules//modules/aws/vpc?ref=v2.3.0"
+  source               = "github.com/answerdigital/terraform-modules//modules/aws/vpc"
   owner                = local.owner
   project_name         = local.project
   enable_vpc_flow_logs = false
@@ -55,6 +59,13 @@ resource "aws_security_group" "aws_sg" {
   }
 
   ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -87,9 +98,9 @@ resource "aws_security_group" "aws_sg" {
   }
 }
 
-module "ec2_instance_setup" {
-  source                 = "github.com/answerdigital/terraform-modules//modules/aws/ec2?ref=TERRA-EC2Hack"
-  project_name           = local.project
+module "kiwi_tcms" {
+  source                 = "github.com/answerdigital/terraform-modules//modules/aws/ec2"
+  project_name           = "kiwi-demo"
   owner                  = local.owner
   ami_id                 = data.aws_ami.ec2_ami.id
   availability_zone      = data.aws_availability_zones.available.names[0]
@@ -116,6 +127,66 @@ sleep 10
 git clone --depth 1 https://github.com/kiwitcms/Kiwi.git path/to/kiwi-tcms/
 cd path/to/kiwi-tcms
 
+/usr/local/bin/docker-compose up
+
+EOF
+
+}
+
+module "it_tools" {
+  source                 = "github.com/answerdigital/terraform-modules//modules/aws/ec2"
+  project_name           = "ittools-demo"
+  owner                  = local.owner
+  ami_id                 = data.aws_ami.ec2_ami.id
+  availability_zone      = data.aws_availability_zones.available.names[0]
+  subnet_id              = module.vpc_subnet.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.aws_sg.id]
+  needs_elastic_ip       = true
+
+  user_data = <<-EOF
+#!/bin/bash -xe
+#logs all user_data commands into a user-data.log file
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -aG docker ec2-user
+sudo newgrp docker
+
+docker run -d --name it-tools --restart unless-stopped -p 8080:80 corentinth/it-tools:latest
+EOF
+
+}
+
+module "sorry_cypress" {
+  source                 = "github.com/answerdigital/terraform-modules//modules/aws/ec2"
+  project_name           = "sorrycypress-demo"
+  owner                  = local.owner
+  ami_id                 = data.aws_ami.ec2_ami.id
+  availability_zone      = data.aws_availability_zones.available.names[0]
+  subnet_id              = module.vpc_subnet.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.aws_sg.id]
+  needs_elastic_ip       = true
+
+  user_data = <<-EOF
+#!/bin/bash -xe
+#logs all user_data commands into a user-data.log file
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install git -y
+sudo yum install docker -y
+sudo service docker start
+sudo usermod -aG docker ec2-user
+sudo newgrp docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sleep 10
+
+curl https://raw.githubusercontent.com/sorry-cypress/sorry-cypress/master/docker-compose.full.yml >> docker-compose.yml
 /usr/local/bin/docker-compose up
 
 EOF
--- output.tf
+++ output.tf
@@ -1,8 +1,26 @@
-output "ip" {
-  value = module.ec2_instance_setup.instance_public_ip_address
+output "kiwi_ip" {
+  value = module.kiwi_tcms.instance_public_ip_address
 }
 
-output "private_key" {
-  value     = module.ec2_instance_setup.private_key
+output "kiwi_private_key" {
+  value     = module.kiwi_tcms.private_key
+  sensitive = true
+}
+
+output "itertools_ip" {
+  value = module.it_tools.instance_public_ip_address
+}
+
+output "itertools_private_key" {
+  value     = module.it_tools.private_key
+  sensitive = true
+}
+
+output "sorrycypress_ip" {
+  value = module.sorry_cypress.instance_public_ip_address
+}
+
+output "sorrycypress_key" {
+  value     = module.sorry_cypress.private_key
   sensitive = true
 }
